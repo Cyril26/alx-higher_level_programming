0935f1afe4a0eda2516786664f7703ce9b7a167c
Joel Cyril Odoom <joel.odoom@petraonline.com>
2023-09-16 11:10:24 +0000

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting
+    with N from hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM state\
+                 WHERE name LIKE BINARY 'N%'\
+                 ORDER BY id ASC")
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+
+    cur.close()
+    db.close()
