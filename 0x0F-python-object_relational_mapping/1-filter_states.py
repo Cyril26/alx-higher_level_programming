2cbef114a3aa670f0ec971e72ff65736549a00e2
Cyril26 <cyrilodm26@gmail.com>
2023-09-16 11:19:07 +0000

    make executable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that lists all states with a name starting
-    with N from hbtn_0e_0_usa"""
+    with N from hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
