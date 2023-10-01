0a2e575bc690836f00f3940ebbcb919fcb6fa288
Cyril26 <cyrilodm26@gmail.com>
2023-10-01 23:12:42 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""""Python script that fetches https://alx-intranet.hbtn.io/status"""
-from urllib import request
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
+
 
 if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        content = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
